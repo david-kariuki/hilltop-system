da6fd9ba89e54b8c474f7e58216b072c460f8828
david-kariuki <dkaris.k@gmail.com>
2020-12-16 23:06:30 +0300

    Database connection functionality.

---
+++ app/php/config/databaseConfiguration.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+* Database configuration file
+* This file contains all the constants required for database connection
+*
+* @author David Kariuki (dk)
+* @copyright (c) 2020 David Kariuki (dk) All Rights Reserved.
+*/
+
+
+/**
+ * Database Configuration
+ */
+define("HOST",          "localhost");
+define("HOST_USERNAME", "");
+define("HOST_PASSWORD", "");
+define("DATABASE_NAME", "");
+
+// EOF: DatabaseConfiguration.php
