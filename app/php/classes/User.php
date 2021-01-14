3e7901af3797efbe34fe0fc64b3dc43370fa5a3e
david-kariuki <dkaris.k@gmail.com>
2021-01-14 14:54:58 +0300

    Added connection instance

--- app/php/classes/User.php
+++ app/php/classes/User.php
@@ -14,12 +14,62 @@
 class User
 {
 
+    // Connection status value variable
+    private $connectToDB;       // Create DatabaseConnection class object
+    private $constants;         // Create Constants class object
+
+
+    /**
+    * Class constructor
+    */
+    function __construct()
+    {
+
+        // Creating objects of the required classes
+
+        // Initialize database connection class instance
+        $connectionInstance = DatabaseConnection::getConnectionInstance();
+
+        // Initialize connection object
+        $this->connectToDB  = $connectionInstance->getDatabaseConnection();
+        $this->constants    = new Constants();      // Initialize constants object
+    }
+
+
+    /**
+    * Class destructor
+    */
+    function __destruct()
+    {
+
+    }
+
     /**
     * Function to add user to the database
     *
     */
     public function addUserToDatabase($userDetails){
-        echo "Add user";
+
+        // UUID
+        firstName
+        lastName
+        otherName
+        gender
+        nationalId
+        Email
+        userName
+        password
+        Address
+        city
+        role
+        status
+        dateCreated
+        lastModified
+        logID
+
+        // Prepare insert statement
+
+
     }
 
     /**
