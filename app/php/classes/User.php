13779645044e287976ff301eb274f5d39148f93d
david-kariuki <dkaris.k@gmail.com>
2021-01-15 12:18:44 +0300

    Development

--- app/php/classes/User.php
+++ app/php/classes/User.php
@@ -132,6 +132,130 @@ class User
     public function deleteUserByReference(){
 
     }
+
+    public function get_by_ref($fields, $table, $reference = null, $type = null ,$limit = null){
+
+        $response = [
+            false,
+        ];
+
+        $tempArray = array();
+
+        // Check if fields is passed as array
+        if (is_array($fields)) {
+
+            $arrayCount = count($fields); // Get array count
+
+            // Check array count length
+            if ($arrayCount > 0) {
+
+                $fieldsCombined = implode(",", $fields); // Join array elements with a comma
+                $stmt; // Shared statement variable
+
+                // Check if reference is set
+                if (isset($reference)) {
+                    // Reference set
+
+                    $keys   = [];   // Keys array
+                    $values = [];   // Values array
+
+                    /**
+                    * Loop through reference using &(reference) to name the value with a different name * or alias. References are a means to access the same variable content by different * names.
+                    *
+                    * @example - Just likes a person who has two different names.
+                    */
+                    foreach ($reference as &$val) {
+
+                        array_push($keys, $val[0] . " = ?"); // Add to keys array
+                        $myVal = $val[1];
+                        array_push($values, $myVal); // Add to values array
+                    }
+
+                    $keysCombined = implode(" AND ", $keys); // Join array elements with a ' AND '
+
+                    // Prepare SELECT statement
+                    $stmt = $this->connectToDB->prepare(
+                        "SELECT $fieldsCombined FROM $table WHERE $keysCombined "
+                    );
+
+                    $stmt->bind_param($type, ...$values); // Bind parameters
+
+                } else {
+                    // Reference not set
+
+                    // Prepare SELECT statement
+                    $stmt = $this->connectToDB->prepare("SELECT $fieldsCombined FROM $table");
+                }
+
+                $stmt->execute(); // Execute statement
+                $result = $stmt->get_result(); // Get result
+                $stmt->close(); // Close statement
+
+                // Loop through result fetching associative array
+                while ($data = $result->fetch_assoc()) {
+
+                    array_push($tempArray, $data); // Add data to the end of temp array
+                    $response[0] = true; // Set response at index 0 to true
+                }
+
+                array_push($response, $tempArray); // Add temp array to response
+            }
+        } else {
+
+            if ($fields == "*") {
+                // Selecting *
+
+                // Check for reference
+                if (isset($reference)) {
+                    // Reference set
+
+                    $keys   = [];   // Keys array
+                    $values = [];   // Values array
+
+                    // Loop through reference
+                    foreach ($reference as &$val) {
+
+                        array_push($keys, $val[0] . " = ?"); // Add to keys array
+                        $myVal = $val[1];
+                        array_push($values, $myVal); // Add to values array
+                    }
+
+                    $keysCombined = implode(" AND ", $keys); // Join array elements with a ' AND '
+
+                    // Create statement
+                    $statement = "select * from tbl_products where " . $keysCombined;
+
+                    echo $statement; // Debug
+                    exit(); // Debug
+
+                    // Prepare SELECT statement
+                    $stmt = $this->connectToDB->prepare("SELECT * FROM $table WHERE $keysCombined ");
+                    $stmt->bind_param($type, ...$values); // Bind parameters
+
+                } else {
+                    // Reference not set
+
+                    // Prepare SELECT statement
+                    $stmt = $this->connectToDB->prepare("SELECT * FROM $table");
+                }
+
+                $stmt->execute(); // Execute statement
+                $result = $stmt->get_result(); // Get result
+                $stmt->close(); // Close statement
+
+                // Loop through result fetching associative array
+                while ($data = $result->fetch_assoc()) {
+
+                    array_push($tempArray, $data); // Add data to the end of temp array
+                    $response[0] = true; // Set response at index 0 to true
+                }
+
+                array_push($response, $tempArray); // Add temp array to response
+            }
+        }
+
+        return $response; // Return response array
+    }
 }
 
 // EOF : User.php
