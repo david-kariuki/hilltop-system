33497a854b0f6bced3ad74408c02524e00302d29
MwauraKimani <kimmwaus@gmail.com>
2021-01-20 10:03:40 +0300

    debug c

--- Views/accounts/control.php
+++ Views/accounts/control.php
@@ -167,7 +167,7 @@ if (isset($_SESSION['TOKEN'])) {
                             "data"=>false
                         );
 
-                        $url = ROOT_DOMAIN."/Views/accounts/accountForm.php";
+                        $url = ROOT."/Views/accounts/accountForm.php";
                         $data = json_encode($user);
 
                         $value = $admin->curl_loader_text_return($url,$data);
