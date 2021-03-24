02243e1a8a33ffbdac8a70556781fcf01571c7dd
MwauraKimani <kimmwaus@gmail.com>
2021-03-24 10:48:52 +0300

    commit to main

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8cf68c38a39537bbb8cadd01f9410c64ab3af0f8',
+    'name' => 'mwaura/hilltop',
+  ),
+  'versions' => 
+  array (
+    'mwaura/hilltop' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8cf68c38a39537bbb8cadd01f9410c64ab3af0f8',
+    ),
+  ),
+);
